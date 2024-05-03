a45af68c3c72bfe41a111d7d8d3e5ade6d7572f8
kinza Amin <kinzaamin530@gmail.com>
2024-05-04 01:30:22 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+	faHome,
+	faSearch,
+	faBell,
+	faEnvelope,
+	faUser,
+} from "@fortawesome/free-solid-svg-icons";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const contentMap = {
+	Home: " this is home page. Lorem Ipsum is simply dummy text of the printing and typesetting industry ",
+	Search:
+		"This is Search content. Filler text is text that shares some characteristics of a real written text.",
+	Notifications:
+		"This is Notifications content. Lorem Ipsum is simply dummy text of the printing and typesetting industry",
+	Messages: "This is Messages content.",
+	Profile: "This is Profile content.",
+};
+
+const App = () => {
+	const [isOpen, setIsOpen] = useState(false);
+	const [headerText, setHeaderText] = useState("Home");
+	const [content, setContent] = useState(contentMap["Home"]);
+
+	const handleMenuToggle = () => {
+		setIsOpen(!isOpen);
+	};
+
+	const handleMenuItemClick = (text, e) => {
+		e.stopPropagation(); 
+		setHeaderText(text);
+		setContent(contentMap[text]);
+		setIsOpen(false);
+	};
+
+	return (
+		<div className='app'>
+			<div className='header'>
+				<h1>{headerText}</h1>
+				<p className='paragraph'>{content}</p>
+			</div>
+			<div
+				className={`menu-container ${isOpen ? "open" : ""}`}
+				onClick={handleMenuToggle}>
+				<div className='menu'>
+					<div
+						className='menu-item'
+						onClick={(e) => handleMenuItemClick("Home", e)}>
+						<FontAwesomeIcon icon={faHome} />
+					</div>
+					<div
+						className='menu-item'
+						onClick={(e) => handleMenuItemClick("Search", e)}>
+						<FontAwesomeIcon icon={faSearch} />
+					</div>
+					<div
+						className='menu-item'
+						onClick={(e) => handleMenuItemClick("Notifications", e)}>
+						<FontAwesomeIcon icon={faBell} />
+					</div>
+					<div
+						className='menu-item'
+						onClick={(e) => handleMenuItemClick("Messages", e)}>
+						<FontAwesomeIcon icon={faEnvelope} />
+					</div>
+					<div
+						className='menu-item'
+						onClick={(e) => handleMenuItemClick("Profile", e)}>
+						<FontAwesomeIcon icon={faUser} />
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
 
 export default App;
